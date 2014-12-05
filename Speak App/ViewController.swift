8026fd438a5181030821dd46292f4d48782aefab
krassik <krassik@mal4o.home>
2014-12-05 14:01:43 -0800

    Shows / hides stop button

--- Speak App/ViewController.swift
+++ Speak App/ViewController.swift
@@ -11,25 +11,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        // Hide the stop button
+        stopButton.hidden = true;
+    }
 
     @IBAction func recordAudio(sender: UIButton) {
         recordingInProgress.hidden = false;
+        stopButton.hidden = false;
         //TODO: record the user's voice
         println("in recordAudio")
     }
     
     @IBAction func stopAudio(sender: UIButton) {
         recordingInProgress.hidden = true;
+        stopButton.hidden = true;
     }
     
 }
