3b12e6bdb94f40d369c49dd96737d240603ac30e
krassik <krassik@mal4o.home>
2014-12-05 23:00:33 -0800

    Implements Vader effect

--- Speak App/PlaySoundsViewController.swift
+++ Speak App/PlaySoundsViewController.swift
@@ -83,6 +83,9 @@ class PlaySoundsViewController: UIViewController {
         playAudioWithVariablePitch(1000)
     }
     
+    @IBAction func playVaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
     
     @IBAction func stopAudio(sender: AnyObject) {
         audioPlayer.stop()
